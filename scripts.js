5ca230fa6fccc426d2dcc4c3d92fa668d4795b42
Nicole Martin <nikismar@hotmail.com>
2017-06-22 14:42:16 -0500

    Part 1 finished - added blocks to the DOM

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log('js sourced');
+
+$(document).ready(function() {
+  console.log("ready function working");
+
+  $('.block').append('<button class="button_red"></button>');
+  $('.block').append('<button class="button_blue"></button>');
+  $('.block').append('<button class="button_green"></button>');
+  $('.block').append('<button class="button_yellow"></button>');
+
+
+});
